a9d8f33ba555b57291bb5b1cc87f6598bf20ed4d
vosstorgg <ryzhkow@gmail.com>
2025-05-24 16:42:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 import openai
 import os
 
-TELEGRAM_TOKEN = os.environ[7890202093:AAHEMHem83hLI5Htpabi06EeGncE0iWEZwI]
-OPENAI_API_KEY = os.environ[REMOVED_API_KEYuxL1Fn7FKxXsAS8kTJ9r2TCEKLKkjQIVSCzUvEZyR1wHBo4OVTW0wIwIjt4ljLUJ3T3BlbkFJJOm2NeZ8gbSn0QzAI9gQNv8ONdm13z2YS8yIN1cdZK0XlaWJOdC2QoiVzmrNlNAKndehx3RpIA]
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 openai.api_key = OPENAI_API_KEY
 
