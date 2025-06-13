12d4af35bcb3f1d36f2e37fa615f61615ba696de
vosstorgg <ryzhkow@gmail.com>
2025-06-13 20:47:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -243,10 +243,10 @@ async def start_first_dream_command(update: Update, context: ContextTypes.DEFAUL
 
 async def channel_view_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Лучшая поддержка сейчас — подписаться на канал автора. Спасибо!",
+        "Лучшая поддержка сейчас — подписаться на канал автора.\n\nСпасибо! ❤️",
         
         reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("Подписаться на канал", url="https://t.me/N_W_passage")]
+                    [InlineKeyboardButton("✅ Подписаться на канал", url="https://t.me/N_W_passage")]
                 ])
     )
 
